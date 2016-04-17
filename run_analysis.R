b379d86f308af5bf3bef63817c764d4a137db14d
JacGu <jcbs46@hotmail.com>
2016-04-18 01:08:04 +0200

    Added files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -1,45 +1,45 @@
-## Merging the trainData set and the testData set.
-## Reading into R the local datasets.
-trainData<-read.table("trainDataset.txt")
-testData<-read.table("testDataset.txt")
+## Reading into R the datasets.
+trainData<-read.table("Y_train.txt")
+testData<-read.table("Y_test.txt")
 ## Merging the 2 datasets with rowbind.
-totalData<-rbind(trainData,testData)
+totalData<-rbind(testData,trainData)
 print(dim(totalData))
 
-## Step two Extracting the average and SD.
-## First, set column names for activity and persons.
-colnames(totalData)[1]<-"act"
-colnames(totalData[2]<-"pers"
-## For oversight I did a sort here, on 1.act and 2.pers.
-totalDataSorted <- totalData %>% arrange(act,pers)
+## Step two Extracting the average and std col's.
+features<-read.table("features.txt")
+meanStdfeatures<-grep("-(mean|std)\\(\\)",features[,2])
+totalDataExtracts<-totalData[,meanStdfeatures]
 
-## Mean and SD.
-sortedMeandataset <-totalDataSorted %>% rowwise() %>% mutate(average=mean(c(V1:V128)))
-meanPlusSddata <-sortedMeandataset %>% rowwise() %>% mutate(stdeviation=sd(c(V1,V2,V3,V4,V5,V6,V7,V8,V9,V10)))
-print(meanPlusSddata[1:5,127:132])
+## Step three var labels.
+varNames<-features[meanStdfeatures]
+colnames(totalDataExtracts)<-varNames[,2]
 
-## Extracting.
-neatData <- select(meanPlusSddata,act,pers,average,stdeviation)
-neatData[1:5,]
-## Add the activity names.
-for ( i in 1:length(neatData$act)){ 
-          if(neatData$act[i]==1){neatData$act[i]<-"WALKING"
-        } else if(neatData$act[i]==2){neatData$act[i]<-"WALKING_UPSTAIRS"
-        } else if(neatData$act[i]==3){neatData$act[i]<-"WALKING_DOWNSTAIRS"
-        } else if(neatData$act[i]==4){neatData$act[i]<-"SITTING"
-        } else if(neatData$act[i]==5){neatData$act[i]<-"STANDING"
-        } else{neatData$act[i]<-"LAYING"
+## Step 4 label activities.
+subjFiletest<-read.table("subject_test.txt")
+subjFiletrain<-read.table("subject_train.text")
+subjData<-rbind(subjFiletest,subjFiletrain)
+
+actTest<-read.table("y_test.txt")
+actTrain<-read.table("y_train.txt")
+actData<-rbind(actTest,actTrain)
+allTogether<-cbind(actData,subjData,totalDataExtracts)
+colnames(allTogether)[1]<-"Activities"
+colnames(allTogether)[2]<-"Person"
+
+## Add appropiate activity names.
+for ( i in 1:length(allTogether$Activities)){ 
+          if(allTogether$Activities[i]==1){allTogether$Activities[i]<-"WALKING"
+        } else if(allTogether$Activities[i]==2){allTogether$Activities[i]<-"WALKING_UPSTAIRS"
+        } else if(allTogether$Activities[i]==3){allTogether$Activities[i]<-"WALKING_DOWNSTAIRS"
+        } else if(allTogether$Activities[i]==4){allTogether$Activities[i]<-"SITTING"
+        } else if(allTogether$Activities[i]==5){allTogether$Activities[i]<-"STANDING"
+        } else{allTogether$Activities[i]<-"LAYING"
         }
 }
-print(neatData[1:5,]
-
-## Appropiate variable names.
-colnames(neatData) <- c("ACTIVITY","PERSON","AVERAGE","STDEVIATION")
-print(neatData[1:5,1:4])
+print(allTogether[1:5,1:7])
 
 ## Final second dataset.
-finalDataset <- select(neatData,ACTIVITY,PERSON,AVERAGE)
-print(finalDataset[1:5,])
-print(finalDataset[92686:92691,])
-
+tidyDataset<-ddply(allTogether,.(Activities,Person), function(x) colMeans(x[,3:68]))
+print(finalDataset[1:5,1:8])
+write.table(tidyDataset,file="tidyDataset.txt",row.name=FALSE)
 
